6988c09c364848119a15437bfa45e9bbc82ec9bc
Jaw <tuyweber@gmail.com>
2024-07-08 21:53:53 -0300

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -5,14 +5,15 @@ import (
 	"log"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
 
 type ResponseStatus string
 
 const (
-	OK        = "200 OK"
-	NOT_FOUND = "404 Not Found"
+	OK        ResponseStatus = "200 OK"
+	NOT_FOUND ResponseStatus = "404 Not Found"
 )
 
 type ServerRequest struct {
@@ -21,13 +22,78 @@ type ServerRequest struct {
 	Version string
 }
 
-func handleRequest(data []byte) ServerRequest {
+type ServerResponse struct {
+	status  ResponseStatus
+	headers map[string]string
+	body    string
+}
+
+func (sr *ServerResponse) SetHeader(header string, value string) *ServerResponse {
+	sr.headers[header] = value
+	return sr
+}
+
+func (sr *ServerResponse) SetBody(body string) *ServerResponse {
+	sr.body = body
+	return sr
+}
+
+func (sr *ServerResponse) SetStatus(status ResponseStatus) *ServerResponse {
+	sr.status = status
+	return sr
+}
+
+func (sr *ServerResponse) ToString() string {
+	headers := ""
+	for key, value := range sr.headers {
+		headers += fmt.Sprintf("%s: %s\r\n", key, value)
+	}
+
+	response := fmt.Sprintf("HTTP/1.1 %s\r\n%s\r\n%s", sr.status, headers, sr.body)
+	return response
+}
+
+func handleRequest(conn net.Conn) {
+	data := make([]byte, 1024)
+	_, err := conn.Read(data)
+
+	if err != nil {
+		log.Fatalln("Error reading from connection", err)
+	}
+
 	parts := strings.Split(string(data), " ")
-	return ServerRequest{
+	request := ServerRequest{
 		Method:  parts[0],
 		Uri:     parts[1],
 		Version: parts[2],
 	}
+
+	response := handlePath(request.Uri)
+	fmt.Println("DEBUG: ", response.ToString())
+
+	_, err = conn.Write([]byte(response.ToString()))
+	if err != nil {
+		log.Fatalln("Error while handling the request:", err)
+	}
+}
+
+func handlePath(path string) ServerResponse {
+	response := ServerResponse{headers: make(map[string]string)}
+	pathParts := strings.Split(path, "/")
+
+	if path == "/" {
+		response.SetStatus(OK)
+	} else if pathParts[1] == "echo" {
+		response.
+			SetHeader("Content-Type", "text/plain").
+			SetHeader("Content-Length", strconv.Itoa(len(pathParts[2]))).
+			SetStatus(OK).
+			SetBody(pathParts[2])
+	} else {
+		response.SetStatus(NOT_FOUND)
+	}
+
+	return response
 }
 
 func writeResponse(conn net.Conn, status ResponseStatus) (int, error) {
@@ -50,19 +116,5 @@ func main() {
 		os.Exit(1)
 	}
 
-	data := make([]byte, 1024)
-	_, err = connection.Read(data)
-	if err != nil {
-		log.Fatalln("Error reading from connection", err)
-	}
-
-	if request := handleRequest(data); request.Uri != "/" {
-		_, err = writeResponse(connection, NOT_FOUND)
-	} else {
-		_, err = writeResponse(connection, OK)
-	}
-
-	if err != nil {
-		log.Fatalln("Couldn't respond the connection!")
-	}
+	handleRequest(connection)
 }
