a4e22940c8350ffbded6b18e89d4866bc2f59c6d
Jaw <tuyweber@gmail.com>
2024-07-08 21:02:40 -0300

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -5,7 +5,34 @@ import (
 	"log"
 	"net"
 	"os"
+	"strings"
 )
+
+type ResponseStatus string
+
+const (
+	OK        = "200 OK"
+	NOT_FOUND = "404 Not Found"
+)
+
+type ServerRequest struct {
+	Method  string
+	Uri     string
+	Version string
+}
+
+func handleRequest(data []byte) ServerRequest {
+	parts := strings.Split(string(data), " ")
+	return ServerRequest{
+		Method:  parts[0],
+		Uri:     parts[1],
+		Version: parts[2],
+	}
+}
+
+func writeResponse(conn net.Conn, status ResponseStatus) (int, error) {
+	return conn.Write([]byte(fmt.Sprintf("HTTP/1.1 %s\r\n\r\n", status)))
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -23,7 +50,18 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = connection.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	data := make([]byte, 1024)
+	_, err = connection.Read(data)
+	if err != nil {
+		log.Fatalln("Error reading from connection", err)
+	}
+
+	if request := handleRequest(data); request.Uri != "/" {
+		_, err = writeResponse(connection, NOT_FOUND)
+	} else {
+		_, err = writeResponse(connection, OK)
+	}
+
 	if err != nil {
 		log.Fatalln("Couldn't respond the connection!")
 	}
